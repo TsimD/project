6ad5c7706806681f0120c01073c57afd81a3d795
iDimit <666.deman.13@gmail.com>
2022-08-18 14:59:09 +0400

    started first programm

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const numberOfFilms= +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors:{},
+    genres:[],
+    private: false,
+
+};
+let filmName;
+let filmPoint;
+for(let i=0; i<2; i++){
+    filmName=prompt("Один из последних просмотренных фильмов?");
+    filmPoint=+prompt("На сколько оцените его?");
+    personalMovieDB.movies[filmName]=filmPoint;
+};
+
+console.log(personalMovieDB);
